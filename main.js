5c46074d4f19510857f072ef171b000ba9d41a1f
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2023-11-23 19:53:44 -0500

    Update on Portfolio Section and creation of Contact Me Section

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 const toggleHome = document.getElementById("main_menu_home");
 const togglePortfolio = document.getElementById("main_menu_portfolio");
 const toggleAbout = document.getElementById("main_menu_aboutme");
+const toggleContact = document.getElementById("main_menu_contact");
 const homeDiv = document.getElementById("home_div");
 const portDiv = document.getElementById("portfolio_div");
 const aboutDiv = document.getElementById("about_me_div");
+const contactDiv = document.getElementById("contact_me_div");
 
 const hide = el => el.style.setProperty("display", "none");
 const show = el => el.style.setProperty("display", "block");
 
 hide(aboutDiv);
 hide(portDiv);
+hide(contactDiv);
 
 toggleHome.addEventListener("click", () => {
     hide(portDiv);
     hide(aboutDiv);
+    hide(contactDiv);
     show(homeDiv);
 });
 
 togglePortfolio.addEventListener("click", () => {
     hide(aboutDiv);
     hide(homeDiv);
+    hide(contactDiv);
     show(portDiv);
 });
 
@@ -27,5 +32,12 @@ toggleAbout.addEventListener("click", () => {
     show(aboutDiv);
     hide(homeDiv);
     hide(portDiv);
+    hide(contactDiv);
 });
 
+toggleContact.addEventListener("click", () => {
+    hide(aboutDiv);
+    hide(homeDiv);
+    hide(portDiv);
+    show(contactDiv);
+});
