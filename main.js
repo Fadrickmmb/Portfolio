be7621fa1565d126659ff6b9dcb7760fb512fd66
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2023-12-12 18:33:15 -0500

    functions for contact form

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const portDiv = document.getElementById("portfolio_div");
 const aboutDiv = document.getElementById("about_me_div");
 const contactDiv = document.getElementById("contact_me_div");
 const quoteDiv = document.getElementById("main_subcontent_02");
+const formButton = document.getElementById("formButton");
+const hiddenInput = document.getElementById("hiddenInput");
 
 const hide = el => el.style.setProperty("display", "none");
 const show = el => el.style.setProperty("display", "block");
@@ -41,6 +43,7 @@ toggleContact.addEventListener("click", () => {
     hide(homeDiv);
     hide(portDiv);
     show(contactDiv);
+    hide(hiddenInput);
 });
 
 function changeLookGif(){
@@ -53,3 +56,31 @@ function changePortColor(color){
 }
 
 
+
+
+
+const publicKey = "wlC3wzB-HWCmX4-Ia";
+const serviceID = "service_0p1b6do";
+const templateID = "template_nlpmrxd";
+
+console.log("test01");
+
+
+    function sendMail(){
+        console.log("test02");
+        var senderName = document.getElementById("nameInput").value;
+        var senderEmail = document.getElementById("emailInput").value;
+        var senderMessage = document.getElementById("messageInput").value;
+        console.log(senderMessage);
+
+        var params = {
+            name_id: senderName,
+            email_id: senderEmail,
+            message_id: senderMessage
+        }
+        console.log("test03");
+    
+        emailjs.send("service_0p1b6do", "template_nlpmrxd", params).then(function (res){
+            alert("Message Sent! Thank you for contacting me. I will get back to you as soon as possible!");
+        })
+    }
