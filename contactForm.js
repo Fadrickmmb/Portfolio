cd21b890b1e8dd2d90a349313368d8640fd1ccfa
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2023-12-14 17:13:29 -0500

    Update to Contact Form

--- contactForm.js
+++ contactForm.js
@@ -1,59 +1,21 @@
-//const contactForm = document.querySelector("#contact_form");
-//const contactName = document.querySelector("#name");
-//const contactEmail = document.querySelector("#email");
-//const contactMessage = document.querySelector("#message");
-//const contactSubmit = document.querySelector("#submitContact");
-
 const publicKey = "wlC3wzB-HWCmX4-Ia";
 const serviceID = "service_0p1b6do";
 const templateID = "template_nlpmrxd";
 
 
-//emailjs.init(publicKey);
-
-
-    console.log("test01");
-
 
     function sendMail(){
-        console.log("test02");
         var senderName = document.getElementById("nameInput").value;
         var senderEmail = document.getElementById("emailInput").value;
         var senderMessage = document.getElementById("messageInput").value;
-        console.log(senderMessage);
 
         var params = {
-            name_id: senderName + "1",
+            name_id: senderName,
             email_id: senderEmail,
             message_id: senderMessage
         }
-        console.log("test03");
     
         emailjs.send("service_0p1b6do", "template_nlpmrxd", params).then(function (res){
-            alert("Success" + res.status);
+            alert("Message Sent! Thank you for contacting me. I will get back to you as soon as possible!");
         })
     }
-
-
-//contactForm.addEventListener("submit", e => {
-//    contactSubmit.innerHTML = "Sending Message";
-//    console.log("Test02");
-
-//    const inputFields = {
-//        name: contactName.value,
-//        email: contactEmail.value,
-//        message: contactMessage.value
-//    }
-
-//    emailjs.send(serviceID, templateID, inputFields)
-//    .then(() => {
-        //contactSubmit.innerHTML = "Message Sent";
-
-//        contactName.value = "";
-//        contactEmail.value = "";
-//        contactMessage.value = "";
-//    }, (error) => {
-//        console.log(error);
-        //contactSubmit.innerHTML = "Something went wrong";
-//    });
-//});
--- main.js
+++ main.js
@@ -58,29 +58,3 @@ function changePortColor(color){
 
 
 
-
-const publicKey = "wlC3wzB-HWCmX4-Ia";
-const serviceID = "service_0p1b6do";
-const templateID = "template_nlpmrxd";
-
-console.log("test01");
-
-
-    function sendMail(){
-        console.log("test02");
-        var senderName = document.getElementById("nameInput").value;
-        var senderEmail = document.getElementById("emailInput").value;
-        var senderMessage = document.getElementById("messageInput").value;
-        console.log(senderMessage);
-
-        var params = {
-            name_id: senderName,
-            email_id: senderEmail,
-            message_id: senderMessage
-        }
-        console.log("test03");
-    
-        emailjs.send("service_0p1b6do", "template_nlpmrxd", params).then(function (res){
-            alert("Message Sent! Thank you for contacting me. I will get back to you as soon as possible!");
-        })
-    }
